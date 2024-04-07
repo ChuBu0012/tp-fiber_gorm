d0da3c638529344a0f6c7e0b56328e8d99923c96
chubu <moomost123@gmail.com>
2024-04-07 18:52:16 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Hello World!",
+		})
+	})
+
+	log.Fatal(app.Listen("localhost:3000"))
+}
